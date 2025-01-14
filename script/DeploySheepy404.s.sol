810fcf183a58d472c366ecb11a62950be3c35fed
Vectorized <webby1111@hotmail.com>
2025-01-14 17:58:30 +0000

    T

--- script/DeploySheepy404.s.sol
+++ script/DeploySheepy404.s.sol
@@ -3,17 +3,12 @@ pragma solidity ^0.8.4;
 
 import {Script} from "forge-std/Script.sol";
 import "../src/Sheepy404.sol";
-import "../src/Sheepy404Mirror.sol";
-import "../src/SheepySale.sol";
 
-contract DeployAllScript is Script {
+contract DeploySheepy404Script is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        bytes32 salt = keccak256(abi.encode(block.timestamp, "hehe"));
-        new Sheepy404{salt: salt}();
-        new Sheepy404Mirror{salt: salt}();
-        new SheepySale{salt: salt}();
+        new Sheepy404();
         vm.stopBroadcast();
     }
 }
